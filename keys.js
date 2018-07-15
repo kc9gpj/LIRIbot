37070246559b18faecf1785854c8426a24361525
david  hoffmann <davidhoffmann@davids-MacBook-Pro.local>
2018-07-15 16:41:24 -0500

    none

--- keys.js
+++ keys.js
@@ -1,13 +1,15 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: '52RpYCfXtzUHGoLVwcJACAk8L',
-  consumer_secret: 'XulFI1HFdMhNUQtAYLORbSAvfz4ib7hHBfRKxj9lsVWKDR7xmL',
-  access_token_key: '344846617-KZCbV8Aad58K5ozOopSpevzPZdbqd16dd1UQtVS6',
-  access_token_secret: '7r0kP8sa77Qobr7QsBMmsdCMLm3KH63pFRDLyhE2kzoO2'
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: '4e61366d2fe54262a751aa55c70a0002',
-  secret: 'd1376bd517314ea78660f596bdf66d27'
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
+
+
