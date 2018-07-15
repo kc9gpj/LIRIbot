2a6d65d148b58de5433a4e0b56abd8c694978660
David Hoffmann <kc9gpj12@gmail.com>
2018-07-15 16:36:10 -0500

    Update keys.js

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: '52RpYCfXtzUHGoLVwcJACAk8L',
+  consumer_secret: 'XulFI1HFdMhNUQtAYLORbSAvfz4ib7hHBfRKxj9lsVWKDR7xmL',
+  access_token_key: '344846617-KZCbV8Aad58K5ozOopSpevzPZdbqd16dd1UQtVS6',
+  access_token_secret: '7r0kP8sa77Qobr7QsBMmsdCMLm3KH63pFRDLyhE2kzoO2'
+};
+
+exports.spotify = {
+  id: '4e61366d2fe54262a751aa55c70a0002',
+  secret: 'd1376bd517314ea78660f596bdf66d27'
+};
